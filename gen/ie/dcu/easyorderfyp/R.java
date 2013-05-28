4be5451c295d3c38a059d820673656ce63e1a9b6
Caoimhin <kevin.conlan2@gmail.com>
2013-05-28 13:46:48 +0100

    LAST EVER COMMIT

--- gen/ie/dcu/easyorderfyp/R.java
+++ gen/ie/dcu/easyorderfyp/R.java
@@ -54,7 +54,7 @@ public final class R {
         public static final int id=0x7f080020;
         public static final int imageView1=0x7f080012;
         public static final int imageView2=0x7f080013;
-        public static final int item=0x7f08001b;
+        public static final int item=0x7f08001a;
         public static final int item_name=0x7f080021;
         public static final int linearLayout1=0x7f080003;
         public static final int logo=0x7f080001;
@@ -63,7 +63,7 @@ public final class R {
         public static final int order_item_list_price=0x7f080025;
         public static final int order_item_list_quantity=0x7f080023;
         public static final int order_list=0x7f080018;
-        public static final int price=0x7f08001a;
+        public static final int price=0x7f08001b;
         public static final int quantity=0x7f080019;
         public static final int textView1=0x7f08000a;
         public static final int tv=0x7f080014;
@@ -107,69 +107,69 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
-        public static final int already_registered=0x7f050006;
         /**  General Strings 
          */
         public static final int app_name=0x7f050000;
-        public static final int eat_in=0x7f050016;
-        public static final int eat_in_instructions=0x7f050027;
         /**  GCM 
+ <string name="error_config">Please set the %1$s constant and recompile the app</string>
+    <string name="already_registered">Device is already registered on server</string>
+    <string name="gcm_registered">From GCM: device successfully registered!</string>
+    <string name="gcm_unregistered">From GCM: device successfully unregistered!</string>
+    <string name="gcm_message">From GCM: you got message!</string>
+    <string name="gcm_error">From GCM: error (%1$s)</string>
+    <string name="gcm_recoverable_error">From GCM: recoverable error (%1$s)</string>
+    <string name="gcm_deleted">From GCM: server deleted %1$d pending messages!</string>
+    <string name="server_registering">Trying (attempt %1$d/%2$d) to register device on Demo Server</string>
+    <string name="server_registered">From Demo Server: successfully added device!</string>
+    <string name="server_unregistered">From Demo Server: successfully removed device!</string>
+    <string name="server_register_error">Could not register device on Demo Server after %1$d attempts</string>
+    <string name="server_unregister_error">Could not unregister device on Demo Server (%1$s)</string>
+    <string name="options_register">Register</string>
+    <string name="options_unregister">Unregister</string>
+    <string name="options_clear">Clear</string>
+    <string name="options_exit">Exit</string> 
          */
-        public static final int error_config=0x7f050005;
+        public static final int eat_in=0x7f050005;
+        public static final int eat_in_instructions=0x7f050016;
         /**  EasyOrder 
          */
-        public static final int full_name=0x7f050022;
-        public static final int gcm_deleted=0x7f05000c;
-        public static final int gcm_error=0x7f05000a;
-        public static final int gcm_message=0x7f050009;
-        public static final int gcm_recoverable_error=0x7f05000b;
-        public static final int gcm_registered=0x7f050007;
-        public static final int gcm_unregistered=0x7f050008;
-        public static final int hello_world=0x7f050033;
-        public static final int items=0x7f050035;
-        public static final int login=0x7f05002f;
+        public static final int full_name=0x7f050011;
+        public static final int hello_world=0x7f050022;
+        public static final int items=0x7f050024;
+        public static final int login=0x7f05001e;
         public static final int menu_settings=0x7f050004;
-        public static final int options_clear=0x7f050014;
-        public static final int options_exit=0x7f050015;
-        public static final int options_register=0x7f050012;
-        public static final int options_unregister=0x7f050013;
-        public static final int order_empty_desc=0x7f050031;
-        public static final int pin=0x7f050023;
-        public static final int previous_orders_txt=0x7f050037;
-        public static final int quantity_dialog_title=0x7f05002d;
-        public static final int register=0x7f050030;
-        public static final int register_login_txt=0x7f050028;
-        public static final int scan_code=0x7f05002a;
-        public static final int scan_instructions=0x7f05002c;
-        public static final int scan_item=0x7f050029;
+        public static final int order_empty_desc=0x7f050020;
+        public static final int pin=0x7f050012;
+        public static final int previous_orders_txt=0x7f050026;
+        public static final int quantity_dialog_title=0x7f05001c;
+        public static final int register=0x7f05001f;
+        public static final int register_login_txt=0x7f050017;
+        public static final int scan_code=0x7f050019;
+        public static final int scan_instructions=0x7f05001b;
+        public static final int scan_item=0x7f050018;
         public static final int scan_items=0x7f050003;
-        public static final int server_register_error=0x7f050010;
-        public static final int server_registered=0x7f05000e;
-        public static final int server_registering=0x7f05000d;
-        public static final int server_unregister_error=0x7f050011;
-        public static final int server_unregistered=0x7f05000f;
-        public static final int submit_order=0x7f05002e;
-        public static final int table_number=0x7f05002b;
-        public static final int take_away=0x7f050017;
-        public static final int title_activity_Register_Activity=0x7f05001c;
-        public static final int title_activity_download_menu_items=0x7f050019;
-        public static final int title_activity_easyorderERROR=0x7f05001f;
-        public static final int title_activity_eat_in=0x7f05001a;
-        public static final int title_activity_logged_out=0x7f050021;
-        public static final int title_activity_login=0x7f050020;
+        public static final int submit_order=0x7f05001d;
+        public static final int table_number=0x7f05001a;
+        public static final int take_away=0x7f050006;
+        public static final int title_activity_Register_Activity=0x7f05000b;
+        public static final int title_activity_download_menu_items=0x7f050008;
+        public static final int title_activity_easyorderERROR=0x7f05000e;
+        public static final int title_activity_eat_in=0x7f050009;
+        public static final int title_activity_logged_out=0x7f050010;
+        public static final int title_activity_login=0x7f05000f;
         /**  Manifest 
          */
-        public static final int title_activity_main=0x7f050018;
-        public static final int title_activity_order_submitted=0x7f05001e;
-        public static final int title_activity_previous_orders=0x7f050036;
-        public static final int title_activity_receipt=0x7f050032;
-        public static final int title_activity_scan_items=0x7f05001d;
-        public static final int title_activity_take_away=0x7f05001b;
+        public static final int title_activity_main=0x7f050007;
+        public static final int title_activity_order_submitted=0x7f05000d;
+        public static final int title_activity_previous_orders=0x7f050025;
+        public static final int title_activity_receipt=0x7f050021;
+        public static final int title_activity_scan_items=0x7f05000c;
+        public static final int title_activity_take_away=0x7f05000a;
         public static final int view_products=0x7f050002;
-        public static final int welcome_header=0x7f050025;
-        public static final int welcome_message=0x7f050024;
-        public static final int welcome_message_main=0x7f050026;
-        public static final int your_order=0x7f050034;
+        public static final int welcome_header=0x7f050014;
+        public static final int welcome_message=0x7f050013;
+        public static final int welcome_message_main=0x7f050015;
+        public static final int your_order=0x7f050023;
     }
     public static final class style {
         /** 
