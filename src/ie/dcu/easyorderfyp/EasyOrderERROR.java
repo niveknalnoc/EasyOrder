4a84674a6309c0fee141bea656a48faaf2a7ed84
Caoimhin <kevin.conlan2@gmail.com>
2013-09-16 22:15:56 +0100

    no changes

--- src/ie/dcu/easyorderfyp/EasyOrderERROR.java
+++ src/ie/dcu/easyorderfyp/EasyOrderERROR.java
@@ -1,6 +1,7 @@
 package ie.dcu.easyorderfyp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class EasyOrderERROR extends Activity {
@@ -9,5 +10,12 @@ public class EasyOrderERROR extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.easyorder_error);
+	}
+	
+	@Override
+	public void onBackPressed() {
+		Intent i = new Intent(getApplicationContext(),
+				LoggedInActivity.class);
+		startActivity(i);
 	}
 }
